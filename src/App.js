4a32aa2f9816b230fa6127f8208a91af33766211
uday <udaykiranvalaboju@gmail.com>
2023-11-06 12:50:07 +0530

    Hero img but font-not done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './Assets/logo.svg';
 import './styles/App.css';
 import Navbar from './Components/Navbar';
+import HeroSection from './Components/HeroCard'
 function App() {
   return (
+    <div>
     <Navbar/>
+    <HeroSection/>
+    </div>
   );
 }
 
---
+++ src/Components/HeroCard/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import './hero.css'
+
+
+const HeroSection = ()=>{
+   return( <div className ='hero'>
+      <img src='heroimg.png' alt='hero-img'className ='hero-img'></img>  
+    </div>);
+}
+
+export default HeroSection;
