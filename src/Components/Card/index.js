602a84101be989b970a726c3194c6b5ea3c3a2d3
uday <udaykiranvalaboju@gmail.com>
2023-11-09 23:20:59 +0530

    Card is implemented

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -5,8 +5,13 @@ import './Card.css'
 export default ()=>{
      return(
         <div className='card'>
-         <img src='' alt='songName'></img>
-         
+         <div className='card-img-content'>
+         <img src='https://i.scdn.co/image/ab67616d0000b273842b1505f3066218e85fc862' alt='songName'></img>
+         <div className='follow-text'>
+            <div>100 Follows</div>
+         </div>
+         </div>
+         <div className='card-tile-name'>Romancham</div>
         </div>
      )
 }
