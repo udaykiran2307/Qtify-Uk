75eb2af210b43481fa1f3943e8edb37abc1ef6fc
uday <udaykiranvalaboju@gmail.com>
2023-11-26 22:56:13 +0530

    some change

--- src/Components/DropDownCard/index.js
+++ src/Components/DropDownCard/index.js
@@ -5,7 +5,7 @@ export default ({ albumData }) => {
     <div className="drop-down-section">
       <div className="img-container">
         <img
-          src="https://img.freepik.com/free-vector/realistic-music-record-label-disk-mockup_1017-33906.jpg?w=826&t=st=1700638188~exp=1700638788~hmac=fc1f75e710b3101f373a8bb2f0f918723e95548161abc356bfacbd3c9feeedf1"
+          src={albumData.image}
           alt="Album Img"
           className="img-class"
         ></img>
